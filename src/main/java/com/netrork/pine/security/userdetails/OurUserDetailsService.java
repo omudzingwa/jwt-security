8a3a11c18044ccadeef22533a333125b3ff2dc39
omudzingwa <obemudzingwa@gmail.com>
2024-05-27 21:22:30 +0200

    Initial commit

--- src/main/java/com/netrork/pine/security/userdetails/OurUserDetailsService.java
+++ src/main/java/com/netrork/pine/security/userdetails/OurUserDetailsService.java
@@ -1,12 +1,12 @@
 package com.netrork.pine.security.userdetails;
 
 import com.netrork.pine.security.roles.Role;
-import com.netrork.pine.security.users.User;
 import com.netrork.pine.security.users.UserRepository;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -28,7 +28,7 @@ public class OurUserDetailsService implements UserDetailsService {
                 .orElseThrow(()->new UsernameNotFoundException("Username not found"));
     }
 
-    private UserDetails createUserDetails(User user) {
+    private UserDetails createUserDetails(com.netrork.pine.security.users.User user) {
         return new org.springframework.security.core.userdetails.User(
                 user.getUsername(),
                 user.getPassword(),
