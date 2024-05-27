e9229045863d9d55c0f2ba78b57b4d6fc9eaaee4
omudzingwa <obemudzingwa@gmail.com>
2024-05-27 20:45:48 +0200

    Initial commit

--- src/main/java/com/netrork/pine/security/logoututils/CustomLogoutHandler.java
+++ src/main/java/com/netrork/pine/security/logoututils/CustomLogoutHandler.java
@@ -11,6 +11,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
 
 @Configuration
@@ -27,17 +28,23 @@ public class CustomLogoutHandler implements LogoutHandler {
         log.info("Length for Token is : " + token.length());
 
         log.info("Fetching token from Database");
-        String tokenFromDatabase = refreshTokenService.findTokenByValue(token).orElseThrow(()-> new RuntimeException("Token not found"));
+        String tokenFromDatabase = refreshTokenService.findTokenByValue(token)
+                .orElseThrow(()-> new RuntimeException("Token not found"));
         log.info("Token from Database is : " + tokenFromDatabase);
         log.info("Token from Request  is : " + token);
 
         //1 - Check if token exists in database
         if(token.equals(tokenFromDatabase)){
+
             long userId=refreshTokenService.findUserIdFromTokenByTokenValue(token);
+
             String username= refreshTokenService.getUsernameForTokenByUserId(userId);
+
             refreshTokenService.deleteTokenByTokenValue(token);
             log.info("Deleted Refresh token for user : " + username);
+
             SecurityContextHolder.clearContext();
+
             try {
                 request.logout();
                 log.info("Logged out User : " + username);
